942781e8bf9549f43bec902d1fa379294c17b805
kimjohnconcha <kimjohnconcha@icloud.com>
2017-05-12 09:12:00 +0800

    Set user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
-Rails.application.routes.draw do
-  
+require 'api_constraints'
+
+RailsApiPoc::Application.routes.draw do
+  devise_for :users
+
+  # namespace :api, defaults: { format: :json }, path: '/' do
+  #   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+  #     resources :users, :only => [:show]
+  #   end
+  # end
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, :only => [:show]
+    end
+  end
+
 end
