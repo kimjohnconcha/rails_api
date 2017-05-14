b43e7ff4ddeadb1cd19f9898dbc9b0130f7f1d48
kimjohnconcha <kimjohnconcha@icloud.com>
2017-05-14 20:18:12 +0800

    Set CRUD features

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 require 'api_constraints'
 
 RailsApiPoc::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
 
-  # namespace :api, defaults: { format: :json }, path: '/' do
+  # namespace :api, defaults: { format: :json } do
   #   scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-  #     resources :users, :only => [:show]
+  #     resources :users, :only => [:show, :create]
   #   end
   # end
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, :only => [:show]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
+
+
 
 end
